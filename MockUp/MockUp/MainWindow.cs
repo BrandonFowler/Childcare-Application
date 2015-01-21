f2219c7fe81c5b29f81bdf656e51d72db6032897
BrandonFowler <brandonf@eagles.ewu.edu>
2015-01-21 03:38:05 -0800

    Very Simple, Child Check In Mock Up, With SQLite
    This is just a very simple mock up program of a child check in interface
    including SQLite. I wrote this mainly as a learning tool to better
    understand C#. Figured I should post it to development, to show some
    evidence of my learning in case anyone cares. For simplicity, the only
    current ID's in the database are 1, 2 , and 3.

---
+++ MockUp/MockUp/MainWindow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+
+namespace MockUp
+{
+    public partial class MainWindow : Form
+    {
+        private Database db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.db = new Database();
+        }
+
+        private void EnterID_Click(object sender, EventArgs e)
+        {
+            Selection.Items.Clear();
+            string id = this.IDInput.Text;
+            this.IDInput.Text = "";
+            String[] names = db.findChildren(id);
+            int i = 0;
+            while (i < names.Length) {
+                Selection.Items.Add(names[i]);
+                i++;
+            }
+        }
+
+        private void Selection_SelectedIndexChanged(object sender, EventArgs e) {
+            CheckIn.Visible = true;
+            CheckOut.Visible = true;
+        }
+
+        private void CheckIn_Click(object sender, EventArgs e) {
+            string selection = Selection.SelectedItems[0].Text;
+            bool checkedIn = this.db.checkIn(selection);
+            Selection.Items.Clear();
+            if (checkedIn){
+                MessageBox.Show(selection + " checked in");
+            }
+            else {
+                MessageBox.Show("Check in failure");
+            }
+        }
+
+        private void CheckOut_Click(object sender, EventArgs e) {
+            string selection = Selection.SelectedItems[0].Text;
+            bool checkedIn = this.db.checkOut(selection);
+            Selection.Items.Clear();
+            if (checkedIn) {
+                MessageBox.Show(selection + " checked out");
+            }
+            else {
+                MessageBox.Show("Check out failure");
+            }
+        }
+
+    }
+}
