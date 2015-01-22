b544e553a1cbb05a0d80f2be1520440bcfc5ed76
blollis <briandlollis@eagles.ewu.edu>
2015-01-22 14:00:28 -0800

    Added window to Edit parent/Child info
    Contains fields for parent/ child info, an add child button and a canvas
    for the parent picture.

--- ChildCareApp/ChildCareApp/win_AdminEditParentInfo.xaml.cs
+++ ChildCareApp/ChildCareApp/win_AdminEditParentInfo.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
-namespace ChildCareApp {
+namespace ChildCareApp
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for win_AdminEditParentInfo.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class win_AdminEditParentInfo : Window
+    {
+        public win_AdminEditParentInfo()
+        {
             InitializeComponent();
-
-
         }
-
     }
 }
