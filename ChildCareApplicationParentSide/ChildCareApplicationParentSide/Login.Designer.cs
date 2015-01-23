2b3d0a766ef6ecea7916ecbedd5a8ccb7654bd69
Brandon Fowler <brandonf@eagles.ewu.edu>
2015-01-22 17:21:07 -0800

    Initialized project for parent side of the application

---
+++ ChildCareApplicationParentSide/ChildCareApplicationParentSide/Login.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace ChildCareApplicationParentSide {
+    partial class frm_Login {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frm_Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frm_Login_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ ChildCareApplicationParentSide/ChildCareApplicationParentSide/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChildCareApplicationParentSide {
+    public partial class frm_Login : Form {
+        public frm_Login() {
+            InitializeComponent();
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
