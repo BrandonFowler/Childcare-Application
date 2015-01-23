ddecdd9eb823c368f0e5e1de90367fdbc6866f0b
blollis <briandlollis@eagles.ewu.edu>
2015-01-23 14:18:11 -0800

    Added to Admin Edit Parent Info

--- ChildCareAppManagerSide/ChildCareApp/win_AdminEditParentInfo.xaml.cs
+++ ChildCareAppManagerSide/ChildCareApp/win_AdminEditParentInfo.xaml.cs
@@ -19,14 +19,33 @@ namespace ChildCareApp {
     public partial class win_AdminEditParentInfo : Window {
         public win_AdminEditParentInfo() {
             InitializeComponent();
-            cnv_ParentIcon.Background = new SolidColorBrush(Colors.Aqua); //setting anvas color so we can see it
+            AddStates();
+            cnv_ParentIcon.Background = new SolidColorBrush(Colors.Aqua); //setting canvas color so we can see it
+            btn_Delete.Background = new SolidColorBrush(Colors.Red); 
         }
 
         private void btn_Submit_Click(object sender, RoutedEventArgs e) {
 
+            bool formNotComplete = true; 
+            formNotComplete = CheckIfNull(); 
+
             //save all information to database
-            
-            ClearFields(); 
+            if (formNotComplete == false)
+            {
+                string firstName, lastName, middle, address, city, state, zip;
+
+                firstName = txt_FirstName.Text;
+                lastName = txt_LastName.Text;
+                middle = txt_MiddleInitial.Text;
+                
+                address = txt_Address.Text;
+                city = txt_City.Text;
+                state = cbo_State.Text; //dont know if this will work yet
+                zip = txt_Zip.Text;
+
+
+                ClearFields();
+            }
 
         }
 
@@ -45,6 +64,116 @@ namespace ChildCareApp {
             txt_IDNumber.Clear();
             txt_PhoneNumber.Clear();
             txt_Zip.Clear();
+        }
+        private bool CheckIfNull() {
+
+            if (string.IsNullOrWhiteSpace(this.txt_Address.Text))
+            {
+                MessageBox.Show("Please enter your address.");
+                return true; 
+            }
+
+            else if (string.IsNullOrWhiteSpace(this.txt_City.Text))
+            {
+                MessageBox.Show("Please enter your city.");
+                return true; 
+            }
+
+            else if (string.IsNullOrWhiteSpace(this.txt_Zip.Text))
+            {
+                MessageBox.Show("Please enter your zip.");
+                return true;     
+            }
+
+            else if (string.IsNullOrWhiteSpace(this.txt_FirstName.Text))
+            {
+                MessageBox.Show("Please enter your first name.");
+                return true;
+            }
+
+            else if (string.IsNullOrWhiteSpace(this.txt_LastName.Text))
+            {
+                MessageBox.Show("Please enter your last name.");
+                return true;
+            }
+
+            else if (string.IsNullOrWhiteSpace(this.txt_MiddleInitial.Text))
+            {
+                MessageBox.Show("Please enter your middle initial.");
+                return true;
+            }
+
+
+            else if (string.IsNullOrWhiteSpace(this.cbo_State.Text))
+            {
+                MessageBox.Show("Please enter your state.");
+                return true;
+            }
+
+            return false; 
+        }
+        private void AddStates() {
+
+            cbo_State.SelectedIndex = 46;
+            
+            cbo_State.Items.Add("AL");
+            cbo_State.Items.Add("AK");
+            cbo_State.Items.Add("AZ");
+            cbo_State.Items.Add("AR");
+            cbo_State.Items.Add("CA");
+            cbo_State.Items.Add("CO");
+            cbo_State.Items.Add("CT");
+            cbo_State.Items.Add("DE");
+            cbo_State.Items.Add("FL");
+            cbo_State.Items.Add("GA");
+
+            cbo_State.Items.Add("HI");
+            cbo_State.Items.Add("ID");
+            cbo_State.Items.Add("IL");
+            cbo_State.Items.Add("IN");
+            cbo_State.Items.Add("IA");
+            cbo_State.Items.Add("KS");
+            cbo_State.Items.Add("KY");
+            cbo_State.Items.Add("LA");
+            cbo_State.Items.Add("ME");
+            cbo_State.Items.Add("MD");
+
+            cbo_State.Items.Add("MA");
+            cbo_State.Items.Add("MI");
+            cbo_State.Items.Add("MN");
+            cbo_State.Items.Add("MS");
+            cbo_State.Items.Add("MO");
+            cbo_State.Items.Add("MT");
+            cbo_State.Items.Add("NE");
+            cbo_State.Items.Add("NV");
+            cbo_State.Items.Add("NH");
+            cbo_State.Items.Add("NJ");
+
+            cbo_State.Items.Add("NM");
+            cbo_State.Items.Add("NY");
+            cbo_State.Items.Add("NC");
+            cbo_State.Items.Add("ND");
+            cbo_State.Items.Add("OH");
+            cbo_State.Items.Add("OK");
+            cbo_State.Items.Add("OR");
+            cbo_State.Items.Add("PA");
+            cbo_State.Items.Add("RI");
+            cbo_State.Items.Add("SC");
+
+            cbo_State.Items.Add("SD");
+            cbo_State.Items.Add("TN");
+            cbo_State.Items.Add("TX");
+            cbo_State.Items.Add("UT");
+            cbo_State.Items.Add("VT");
+            cbo_State.Items.Add("VA");
+            cbo_State.Items.Add("WA");
+            cbo_State.Items.Add("WV");
+            cbo_State.Items.Add("WI");
+            cbo_State.Items.Add("WY");
+
+
+
+
         }
     }
 }
