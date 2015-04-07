21074adc66b289f17c5ca751cd7b253ff84c076f
BrandonFowler <brandonf@eagles.ewu.edu>
2015-04-06 17:03:36 -0700

    Added a couple children to make my testing faster

---
+++ ChildcareApplication/ChildcareApplication/Database/BrandonsEdgeCases.sql
@@ -0,0 +1,8 @@
+
+INSERT into AllowedConnections VALUES ("000002", "123450", "123452", "12345", NULL);
+
+INSERT into AllowedConnections VALUES ("000003", "123450", "123453", "12345", NULL);
+
+INSERT into Child VALUES ("123452", "Smeagol", "Golem", '2015-03-25', "Rings of Power", "Addicted to the Ring of power", "../../Pictures/Smeagol.jpg", null);
+
+INSERT into Child VALUES ("123453", "Tauriel", "Silvan", '2013-01-15', "Authority", "Broken Heart", "../../Pictures/Tauriel.jpg", null);
