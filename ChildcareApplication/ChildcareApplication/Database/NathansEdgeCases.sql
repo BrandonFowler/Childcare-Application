e4911f4614eaa8493cca62575f8ca1136a419541
BrandonFowler <brandonf@eagles.ewu.edu>
2015-04-06 19:50:55 -0700

    Database Changes
    Made changes required for new functionality requested by Tracey. Please
    see E-mail!

--- ChildcareApplication/ChildcareApplication/Database/NathansEdgeCases.sql
+++ ChildcareApplication/ChildcareApplication/Database/NathansEdgeCases.sql
@@ -1,8 +1,8 @@
 INSERT into Guardian VALUES ("123450", "1234", "Bilbo", "Baggins", "555-123-4567", "myprecious@theshire.com", "123 Bag End", "Hole A", "Shire", "ME", "99999", "../../Pictures/Bilbo_Baggins.jpg", null);
 				
-INSERT into Child VALUES ("123451", "Frodo", "Baggins", 2005-02-15, "Rings of Power", "Addicted to the Ring of power", "../../Pictures/Frodo_Baggins.jpg", null);
+INSERT into Child VALUES ("123451", "Frodo", "Baggins", '2005-02-15', "Rings of Power", "Addicted to the Ring of power", "../../Pictures/Frodo_Baggins.jpg", null);
 
-INSERT into AllowedConnections VALUES ("000001", "123450", "123451", "12345");
+INSERT into AllowedConnections VALUES ("000001", "123450", "123451", "12345", NULL);
 
 Insert into ChildcareTransaction VALUES ("0000000001","Regular Childcare", "000001", '2015-03-12 12:00:00', '12:00:00', '14:00:00', 10.00);
 
---
+++ ChildcareApplication/ChildcareApplication/Database/childcare_v7/childcare_v7_startingdata.sql
@@ -0,0 +1,117 @@
+INSERT INTO Administrator
+VALUES ("a",
+	"a",
+	"1",
+	"tester@testmail.test",
+	NULL);
+
+INSERT INTO EventData
+VALUES ("Late Fee",
+        30,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+	NULL,
+		NULL);
+
+INSERT INTO EventData
+VALUES ("Regular Childcare",
+        5,
+        2.25,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+	3,
+		NULL);
+
+INSERT INTO EventData
+VALUES ("Adolescent Childcare",
+        0,
+        0,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+	3,
+		NULL);
+
+INSERT INTO EventData
+VALUES ("Infant Childcare",
+        6,
+        3.25,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+	2,
+		NULL);
+
+INSERT INTO EventData
+VALUES ("Parent's Night Out",
+        NULL,
+        NULL,
+        18,
+        7,
+        NULL,
+        NULL,
+        "Friday",
+	NULL,
+		NULL);
+
+INSERT INTO EventData
+VALUES ("Camp",
+        NULL,
+        NULL,
+        36,
+        26,
+        NULL,
+        NULL,
+        NULL,
+	NULL,
+		NULL);
+
+INSERT INTO OperatingHours
+VALUES ("Monday", '08:00:00', '19:00:00');
+
+INSERT INTO OperatingHours
+VALUES ("Tuesday", '08:00:00', '19:00:00');
+
+INSERT INTO OperatingHours
+VALUES ("Wednesday", '08:00:00', '19:00:00');
+
+INSERT INTO OperatingHours
+VALUES ("Thursday", '08:00:00', '19:00:00');
+
+INSERT INTO OperatingHours
+VALUES ("Friday", '08:00:00', '20:00:00');
+
+INSERT INTO OperatingHours
+VALUES ("Saturday", '08:00:00', '12:30:00');
+
+INSERT INTO ApplicationSettings
+VALUES ("Billing Start Date", "20");
+
+INSERT INTO ApplicationSettings
+VALUES ("Billing End Date", "19");
+
+INSERT INTO ApplicationSettings
+VALUES ("Regular Care Maximum Monthly Charge", "100");
+
+INSERT INTO ApplicationSettings
+VALUES ("Days to Hold Expired and Deleted Records", "365");
+
+INSERT INTO ApplicationSettings
+VALUES ("Maximum Number of Hours Charged for Childcare", "3");
+
+INSERT INTO ApplicationSettings
+VALUES ("Infant Age End", "1");
+
+INSERT INTO ApplicationSettings
+VALUES ("Regular Age End", "7");
