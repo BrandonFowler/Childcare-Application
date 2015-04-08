c744f1c7571cb4678a6b433c50c22c848dacc74e
blollis <briandlollis@eagles.ewu.edu>
2015-04-08 15:47:36 -0700

    Starting Hashing test
    Had to change the Admin a password (in the database) to the MD5 hash for
    a. Added hashing.cs

---
+++ ChildcareApplication/ChildcareApplication/AdminTools/Hashing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChildcareApplication.AdminTools
+{
+    class Hashing
+    {
+
+        public static string HashPass(string pass)
+        {
+
+            string sSourceData;
+            byte[] tmpSource;
+            byte[] tmpHash;
+
+            string hashed;
+            //Create a byte array from source data
+            sSourceData = pass;
+            tmpSource = ASCIIEncoding.ASCII.GetBytes(sSourceData);
+
+            //Compute hash based on source data
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            hashed = ByteArrayToString(tmpHash);
+
+            return hashed;
+
+
+        }
+
+        static string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length - 1; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+    }
+}
