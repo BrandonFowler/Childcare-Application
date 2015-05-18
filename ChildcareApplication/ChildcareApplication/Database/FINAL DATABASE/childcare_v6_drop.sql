3400c59bd241daa64f91a34e54a9ed0b096c5ece
Blaine Abbott <abbottw@eagles.ewu.edu>
2015-05-18 01:05:59 -0700

    final database schema diagram
    4 files in a new subfolder called FINAL DATABASE
    -the diagram showing the final schema
    -create and drop sql for the database
    -the xml file that can be imported into vertabelo (possibly of use to
    future developers)

---
+++ ChildcareApplication/ChildcareApplication/Database/FINAL DATABASE/childcare_v6_drop.sql
@@ -0,0 +1,25 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-05-18 08:02:49.434
+
+
+
+-- tables
+-- Table: Administrator
+DROP TABLE Administrator;
+-- Table: AllowedConnections
+DROP TABLE AllowedConnections;
+-- Table: Child
+DROP TABLE Child;
+-- Table: ChildcareTransaction
+DROP TABLE ChildcareTransaction;
+-- Table: EventData
+DROP TABLE EventData;
+-- Table: Family
+DROP TABLE Family;
+-- Table: Guardian
+DROP TABLE Guardian;
+
+
+
+-- End of file.
+
