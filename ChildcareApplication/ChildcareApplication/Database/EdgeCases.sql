33e5a498fabab00fbd09b859c1ebeccc75bcb572
nrgraham23 <nrgraham23@gmail.com>
2015-05-18 13:28:48 -0700

    Database folder cleanup
    Removed some out of date data.

--- ChildcareApplication/ChildcareApplication/Database/EdgeCases.sql
+++ ChildcareApplication/ChildcareApplication/Database/EdgeCases.sql
@@ -92,3 +92,15 @@ INSERT into AllowedConnections VALUES ("001001", "100041", "100042", "10004", NU
 Insert into Family VALUES ('10004', 51.65, 24.25, 30.02);
 
 Insert into Family VALUES ('12345', 51.65, 24.25, 30.02);
+
+INSERT into Guardian VALUES ("123451", "81DC9BDB52D04DC20036DBD8313ED0", "Belba", "Baggins", "555-123-4567", "something@theshire.com", "123 Bag End", "Hole A", "Shire", "ME", "99999", "../../Pictures/Parent.jpg", null);
+
+INSERT into AllowedConnections VALUES ("000002", "123450", "123452", "12345", NULL);
+
+INSERT into AllowedConnections VALUES ("000003", "123450", "123453", "12345", NULL);
+
+INSERT into AllowedConnections VALUES ("000004", "123451", "123451", "12345", NULL);
+
+INSERT into Child VALUES ("123452", "Smeagol", "Golem", '2015-03-25', "Rings of Power", "Addicted to the Ring of power", "../../Pictures/Smeagol.jpg", null);
+
+INSERT into Child VALUES ("123453", "Tauriel", "Silvan", '2013-01-15', "Authority", "Broken Heart", "../../Pictures/Tauriel.jpg", null);
